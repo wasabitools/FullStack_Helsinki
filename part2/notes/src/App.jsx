d59a01107e8086f35f6dde8b5ce65b27f8af999d
natasafi <natasafrag@gmail.com>
2024-10-01 18:32:56 +0100

    introduces axios

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,11 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Note } from './components/Note'
 import { Form } from './components/Form'
 import { Button } from './components/Button'
 
 const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fullfiled')
+        setNotes(response.data)
+      })
+  }
+
+  useEffect(hook, [])
 
   const addNote = (newNote) => {
     //create a new note Object then with the setter add it to the previous notes
